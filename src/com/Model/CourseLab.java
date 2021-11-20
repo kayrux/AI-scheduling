3dfcb55cadca86965e7cf581025ca346e2b85aa2
kayrux <skusjanto@outlook.com>
2021-11-20 16:12:56 -0700

    Add code for the data class CourseLab

--- src/com/Model/CourseLab.java
+++ src/com/Model/CourseLab.java
@@ -2,4 +2,25 @@ package com.Model;
 
 public class CourseLab {
 
+	private String name;
+	private int lectureNumber;
+	private int labNumber;
+	
+	public CourseLab(String name, int lectureNumber, int labNumber) {
+		this.name = name;
+		this.lectureNumber = lectureNumber;
+		this.labNumber = labNumber;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public int getLectureNumber() {
+		return this.lectureNumber;
+	}
+	
+	public int getLabNumber() {
+		return this.labNumber;
+	}
 }
