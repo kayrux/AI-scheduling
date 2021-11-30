c7d7447b9789ae94f7164cd9f59bf666a20ff277
Sherriff Kadiri <sherriff.kadiri@ucalgary.ca>
2021-11-30 10:57:07 -0700

    Populate

--- src/com/OrModel/Populate.java
+++ src/com/OrModel/Populate.java
@@ -1,91 +1,36 @@
-package OrModel;
-
+package com.OrModel;
 import java.util.ArrayList;
-import com.Model.*;
-import java.lang.Math;
+import com.DataStructures.*;
 
-public class Populate {
-//ArrayList<ArrayList<Slot>> facts = new ArrayList<>();
-Node facts;
-
-    public ArrayList<ArrayList<Slot>> PopulateNoTree (ArrayList<Slot> slotList,
-	ArrayList<CourseLab>,
-	ArrayList<Pair<CourseLab, CourseLab>> notCompatibleList,
-	ArrayList<Slot> initFact,
-	int factCount) {
-		ArrayList<Slot> fact = new ArrayList<Slot>();
-		ArrayList<ArrayList<Slot>> factList = new ArrayList<ArrayList<Slot>>();
-		for(int i = 0; i < factCount; i++) {
-			for(int j = 0; j < 3; j++) {
-				makeFact(slotList, initFact);
-			}
-			factList.add(fact);
-		}
-		return factList;
-    }
+class Populate {
 	
-	public ArrayList<Slot> makeFact(ArrayList<Slot> slotList, ArrayList<Slot> initFact) {
-		if(initFact.size() == 0) {
-			ArrayList<Slot> fact = new ArrayList<Slot>();
-		} else {
-			ArrayList<Slot> fact = new ArrayList<Slot>(initFact);
-		}
+	public static Boolean constraintStart() {
+		return true;
+	}
+
+	public static void PopulateOrTree (ArrayList<CourseLab> courseLabs, ArrayList<Slot> slotList,
+											ArrayList<Pair<CourseLab, CourseLab>> noncompatibleArray,
+											ArrayList<Pair<CourseLab, Slot>> unwantedArray) {
 		ArrayList<Slot> fact = new ArrayList<Slot>();
 		int randSlot = (int)(Math.random() * slotList.size());
-		fact.add(slotList.get(randSlot));
-		if(constraint(fact) == True) {
-			return fact;
-		} else {
-			makeFact(slotList, initFact);
+		NodeTemp curNode = new NodeTemp(slotList.get(randSlot), null);
+		NodeTemp root = new NodeTemp(curNode.value, null);
+		int child = 0;
+		while(true) {
+			randSlot = (int)(Math.random() * slotList.size());
+			curNode.addChild(slotList.get(randSlot));
+			child++;
+			if(constraintStart() == true) {
+				fact.add(slotList.get(randSlot));
+				curNode = new NodeTemp(curNode.children.get(child).value, curNode.value);
+			} else {
+				curNode.backTrack();
+				if(child > curNode.children.size()) {
+					curNode.backTrack();
+					child = curNode.children.size();
+				}
+			}
+			
 		}
 	}
-	
-	////////////////////
-	//Using an Or Tree
-	
-	public ArrayList<ArrayList<Slot>> PopulateOrTree () {
-		
-	}
-}
-
-public class Node {
-    Slot value;
-    ArrayList<Node> children = new ArrayList<Node>();
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-
-    Node(Slot value) {
-        this.value = value;
-        children = null;
-    }
-
-    public Node addChild(Node parent, Slot child) {
-        parent.children.add(Node(child));
-		return Node;
-    }
-	
-	public checkConstraints() {
-		
-	}
-	
-	public buildTree(Node root) {
-		int first = 0;
-		while(first != 1) {
-			addChild(root, );
-			checkConstraints(Node);
-		}
-	}
-	
-}
-
-public class TreeOr {
-    ArrayList<TreeOr> tree = new ArrayList<TreeOr>();
 }
