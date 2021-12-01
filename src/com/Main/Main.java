2fe289ab3bc4752e8838c9cdabe2edee8e69b7c7
kayrux <skusjanto@outlook.com>
2021-11-30 22:15:42 -0700

    Merge

--- src/com/Main/Main.java
+++ src/com/Main/Main.java
@@ -2,12 +2,9 @@ package com.Main;
 
 import java.util.ArrayList;
 
-import com.Constants.ValidTimeSlots;
 import com.DataStructures.CourseLab;
-import com.DataStructures.EmptySlot;
 import com.DataStructures.Pair;
 import com.DataStructures.Slot;
-import com.DataStructures.SlotArrayListWrapper;
 import com.DataStructures.Triplet;
 import com.Model.SetbasedSearch;
 import com.OrModel.*;
@@ -15,7 +12,6 @@ import com.OrModel.*;
 public class Main {
 	public static void main(String[] args) {
 		Parser parser = new Parser();
-<<<<<<< HEAD
 		SetbasedSearch setBasedSearch;
 		
 		String txtfile = "res/ShortExample.txt"; // Example file
@@ -33,26 +29,10 @@ public class Main {
         
         ArrayList<Slot> sol = setBasedSearch.search();
         
-=======
-		SetbasedSearch search;
+        // TESTING
 		ArrayList<Slot> temp;
-		
-		String txtfile = "res/ShortExample.txt"; // Example file
-
-        ArrayList<Slot> slotArray = Parser.parseCourseLabSlots(txtfile);
-        ArrayList<CourseLab> courseLabArray = Parser.parseCourseLab(txtfile);
-        ArrayList<Pair<CourseLab, CourseLab>> notCompatibleArray = Parser.parseNotCompatible(txtfile);
-        ArrayList<Pair<CourseLab, Slot>> unwantedArray = Parser.parseUnwanted(txtfile);
-        ArrayList<Triplet<Slot, CourseLab, Integer>> preferencesArray = Parser.parsePreferences(txtfile);
-        ArrayList<Pair<CourseLab, CourseLab>> pairArray = Parser.parsePair(txtfile);
-        ArrayList<Pair<CourseLab, Slot>> partialAssignArray = Parser.parsePartialAssignments(txtfile);
-        
-        search = new SetbasedSearch(slotArray, courseLabArray, notCompatibleArray, unwantedArray, preferencesArray, pairArray, partialAssignArray);
 		temp = Populate.PopulateOrTree(courseLabArray, slotArray, notCompatibleArray, unwantedArray, partialAssignArray);
-		//for(int i = 0; i < temp.size(); i++) {
-		//	System.out.print();
-		//}
->>>>>>> f3fca527a8750e11f572f85cf1ff8345119e92ae
+
 	}
 	
 
